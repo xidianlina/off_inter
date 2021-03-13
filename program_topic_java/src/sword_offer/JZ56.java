6c191880252546349f0f89e83f8edd8b78780cc7
lina.2017 <lina.2017@bytedance.com>
2021-03-13 22:26:13 +0800

    jz60

---
+++ program_topic_java/src/sword_offer/JZ56.java
@@ -0,0 +1,43 @@
+package sword_offer;
+
+/**
+ * 题目描述
+ * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
+ * 示例1
+ * 输入
+ * {1,2,3,3,4,4,5}
+ * 返回值
+ * {1,2,5}
+ */
+public class JZ56 {
+    public ListNode deleteDuplication(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head != null && head.next == null) {
+            return head;
+        }
+
+        ListNode first = new ListNode(-1);
+        first.next = head;
+        ListNode last = first;
+        ListNode p = head;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                int val = p.val;
+                while (p != null & p.val == val) {
+                    if (p.val == val) {
+                        p = p.next;
+                    }
+                }
+                last.next = p;
+            } else {
+                last = p;
+                p = p.next;
+            }
+        }
+
+        return first.next;
+    }
+}
