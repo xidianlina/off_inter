8522dcea45e11c7648b30adbe76885f41942f405
李娜 <lina.2017@bytedance.com>
2021-03-14 21:48:05 +0800

    order2

---
+++ program_topic_java/src/others/order/BinaryInsertSort.java
@@ -0,0 +1,51 @@
+package others.order;
+
+//二分插入排序
+public class BinaryInsertSort {
+    public static void main(String[] args) {
+        BinaryInsertSort binaryInsertSort = new BinaryInsertSort();
+        int[] arr = {8, 0, 5, 7, 9, 1, 4, 2, 6, 3};
+        binaryInsertSort.printArray(arr);
+        binaryInsertSort.binaryInsertSort(arr);
+        binaryInsertSort.printArray(arr);
+    }
+
+    public void printArray(int[] arr) {
+        int size = arr.length;
+        if (size == 0 || arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println(arr[size - 1]);
+    }
+
+    public void binaryInsertSort(int[] arr) {
+        int size = arr.length;
+        if (size == 0 || arr == null) {
+            return;
+        }
+
+        for (int i = 1; i < size; i++) {
+            int k = arr[i];
+            int left = 0;
+            int right = i - 1;
+            int mid = 0;
+            while (left <= right) {
+                mid = (left + right) >> 1;
+                if (k < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            for (int j = i - 1; j >= left; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[left] = k;
+        }
+    }
+}
---
+++ program_topic_java/src/others/order/InsertSort.java
@@ -0,0 +1,42 @@
+package others.order;
+
+//直接插入排序
+public class InsertSort {
+    public static void main(String[] args) {
+        InsertSort insertSort = new InsertSort();
+        int[] arr = {8, 0, 5, 7, 9, 1, 4, 2, 6, 3};
+        insertSort.printArray(arr);
+
+        insertSort.insertSort(arr);
+        insertSort.printArray(arr);
+    }
+
+    public void printArray( int[] arr) {
+        int size = arr.length;
+        if (size == 0 || arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println(arr[size - 1]);
+    }
+
+    public void insertSort(int[] arr) {
+        int size = arr.length;
+        if (size == 0 || arr == null) {
+            return;
+        }
+
+        for (int i = 1; i < size; i++) {
+            int k = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > k) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = k;
+        }
+    }
+}
