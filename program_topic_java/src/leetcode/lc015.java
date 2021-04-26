03c7f2899df88da86917fac6f9a0d23f985a9e94
李娜 <lina.2017@bytedance.com>
2021-04-26 14:21:42 +0800

    lc015

--- program_topic_java/src/leetcode/lc015.java
+++ program_topic_java/src/leetcode/lc015.java
@@ -26,41 +26,46 @@ import java.util.List;
  */
 public class lc015 {
     public static void main(String[] args) {
-
+        lc015 lc = new lc015();
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        System.out.println(lc.threeSum(arr));
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
         int target = 0;
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<>();
         int size = nums.length;
         if (size < 3 || nums == null) {
             return res;
         }
+
         Arrays.sort(nums);
         for (int i = 0; i < size - 2; ++i) {
             //去重
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int l = i + 1;
-            int r = size - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
+
+            int left = i + 1;
+            int right = size - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum < target) {
-                    while (l < r && nums[++l] == nums[l - 1]) ;
+                    while (left < right && nums[++left] == nums[left - 1]) ;
                 } else if (sum > target) {
-                    while (l < r && nums[--r] == nums[r + 1]) ;
+                    while (left < right && nums[--right] == nums[right + 1]) ;
                 } else {
-                    List<Integer> tmp = new ArrayList<Integer>();
+                    List<Integer> tmp = new ArrayList<>();
                     tmp.add(nums[i]);
-                    tmp.add(nums[l]);
-                    tmp.add(nums[r]);
+                    tmp.add(nums[left]);
+                    tmp.add(nums[right]);
                     res.add(tmp);
-                    while (l < r && nums[++l] == nums[l - 1]) ;
-                    while (l < r && nums[--r] == nums[r + 1]) ;
+                    while (left < right && nums[++left] == nums[left - 1]) ;
+                    while (left < right && nums[--right] == nums[right + 1]) ;
                 }
             }
         }
+
         return res;
     }
 }
