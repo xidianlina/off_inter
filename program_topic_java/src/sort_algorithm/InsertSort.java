201ae404d875fac5890e41ef74168478a18da72c
李娜 <lina.2017@bytedance.com>
2021-05-08 21:32:00 +0800

    inset_sort

--- program_topic_java/src/sort_algorithm/InsertSort.java
+++ program_topic_java/src/sort_algorithm/InsertSort.java
@@ -1,16 +1,3 @@
-9种排序算法
-======
-> 排序算法总结：                   
-  插入排序：直接插入排序，二分（折半）插入排序，希尔排序               
-  交换排序：冒泡排序，快速排序            
-  选择排序：简单选择排序，堆排序               
-  归并排序              
-  基数排序              
-  计数排序          
-> ![sort](http://github.com/xidianlina/off_inter/raw/master//program_topic_java/src/sort_algorithm/picture/sort.png)            
-# 插入排序
-## 直接插入排序
-```java
 package sort_algorithm;
 
 //直接插入排序
@@ -52,5 +39,3 @@ public class InsertSort {
         System.out.println(arr[size - 1]);
     }
 }
-```
-## 二分插入排序
